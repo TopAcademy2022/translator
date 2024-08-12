d5f19090774528abb8e2ac0b5b8c636283747959
artemqdh <161634897+artemqdh@users.noreply.github.com>
2024-08-12 20:20:50 +0300

    Update Translate.cs

--- translator/Translate.cs
+++ translator/Translate.cs
@@ -24,7 +24,7 @@
 				Console.WriteLine("4. Delete word from dictionary");
 				Console.WriteLine("5. Delete word from dictionary");
 
-				Console.Write("Выберите пункт меню: ");
+				Console.Write("Выберите пункт меню: \n");
 				ConsoleKeyInfo userInputSymbol;
 
 				do
