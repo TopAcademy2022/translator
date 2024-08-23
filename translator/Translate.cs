d79edeca5dc47b4d967225ff8734a7a44ba3a8b4
artemqdh <161634897+artemqdh@users.noreply.github.com>
2024-08-23 20:06:42 +0300

    Added method save dictionary to file. Closed #13 (#20)

--- translator/Translate.cs
+++ translator/Translate.cs
@@ -24,6 +24,7 @@
 				Console.WriteLine("4. Delete word from dictionary");
 				Console.WriteLine("5. Find word from dictionary");
 				Console.WriteLine("6. Load dictionary from file");
+				Console.WriteLine("7. Save dictionary to file");
 
 				Console.WriteLine("Выберите пункт меню:");
 				ConsoleKeyInfo userInputSymbol;
@@ -89,6 +90,21 @@
 						}
 		 
 						break;
+
+					case 7:
+						Console.WriteLine("Set file name:");
+						string fileName = Console.ReadLine();
+
+						if (this.SaveFile(fileName))
+						{
+							Console.WriteLine($"The File {fileName} was saved successfully.");
+						}
+						else
+						{
+							Console.WriteLine($"Error in saving.");
+						}
+
+						break;
 				}
 			}
 		}
@@ -143,6 +159,38 @@
 				} //!< Dictionary entry
 
 				return true;
+			}
+
+			return false;
+		}
+
+		/*!
+		 * @brief Saving dictionary to file.
+		 * @param[in] Including fileName as new name of the saved file.
+		 * @param[in] Setting folderName and format as default manually.
+		 * @return True - File was successfully saved; False - Error in saving file.
+		 */
+		private bool SaveFile(string fileName, string folderName = "DictionaryTranslate", string format = ".lge")
+		{
+			// Getting Path of File
+			string filePath = Path.Combine(Path.Combine(Directory.GetCurrentDirectory(), folderName), fileName + format);
+
+			try
+			{
+				// Write dictionary to file
+				using (StreamWriter writer = new StreamWriter(filePath)) 
+				{
+					foreach (KeyValuePair<string, string> i in _dictionaryTranslate)
+					{
+						writer.WriteLine($"{i.Key}-{i.Value}");
+					}
+				}
+
+				return true;
+			}
+			catch (Exception problem)
+			{
+				Console.WriteLine($"Something went wrong in saving {fileName}. Error: {problem}.");
 			}
 
 			return false;
