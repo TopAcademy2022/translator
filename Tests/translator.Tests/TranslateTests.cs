fed2c9dab204c86399d1c172d72bb5093f970e0f
Kasyanov-Igor <igor.kasyanov00@mail.ru>
2024-09-01 14:20:16 +0300

    Added test for FindWord method. Closed #28

--- Tests/translator.Tests/TranslateTests.cs
+++ Tests/translator.Tests/TranslateTests.cs
@@ -1,7 +1,19 @@
 namespace translator.Tests
 {
-    public class TranslateTests
-    {
-      
-    }
+	public class TranslateTests
+	{
+		[Fact]
+		/*! 
+		* @brief Checking the word search method.
+		*/
+		public void FindWordTest()
+		{
+			Translate test = new Translate(); //< The creation of our class
+
+            string key = "hello";
+			test.AddWord(key, "привет"); //< Adding a word
+
+            Assert.True(test.FindWord(key) && test.DictionaryTranslate.ContainsKey(key)); //< Checking the search method
+		}
+	}
 }
